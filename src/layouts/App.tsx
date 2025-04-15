aaa606b541f19e87dda79be444860d922ed70375
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-04-14 22:44:26 -0500

    feat: agregar lógica para manejar filas dinámicas y datos en la tabla de programación

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,20 +1,69 @@
+import { meses, headers, participantes } from "../utils/data"
+
 import { useState } from "react"
 
-const meses = [
-  "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
-];
+interface IItem {
+  id: number
+  fecha: string,
+  hora: string,
+  tipo: string,
+  director: string,
+  predicador: string
+}
+
 
 function App() {
 
   const [code, setCode] = useState<string>('C22')
   const [month, setMonth] = useState<string>(meses[new Date().getMonth()])
   const [year, setYear] = useState<string>(new Date().getFullYear().toString())
+  const [filas, setFilas] = useState(1)
+
+  const [items, setItems] = useState<IItem[]>([{
+    id: 0,
+    fecha: "",
+    hora: "",
+    tipo: "",
+    director: "",
+    predicador: ""
+  }])
 
   function handleSchedule() {
     console.log("codigo: ", code)
     console.log("mes: ", month)
     console.log("año: ", year)
+
+    console.log(items)
+  }
+
+  function agregarFila() {
+    setFilas(filas+1)
+    const nuevoItem: IItem = {
+      id: filas,
+      fecha: "",
+      hora: "",
+      tipo: "",
+      director: "",
+      predicador: ""
+    }
+    setItems([...items, nuevoItem])
+  }
+
+  function modificar(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>, campo: keyof IItem, item: IItem) {
+    const nuevosItems = items.map((i: IItem) => 
+      i.id === item.id ? { ...i, [campo]: e.target.value } : i
+    );
+    setItems(nuevosItems);
+  }
+
+  function eliminarFila(id: number) {
+    if (filas === 1) {
+      alert("No puedes eliminar la última fila")
+      return;
+    }
+    console.log(id)
+    const nuevosItems = items.filter(item => item.id !== id);
+    setItems(nuevosItems);
   }
 
   return (
@@ -65,24 +114,98 @@ function App() {
         </label>
       </div>
 
-      <div id="tabla">
-        <table className="border-2 border-gray-300 rounded-md p-2 mt-2 border-collapse">
+      <div id="tabla" className="w-full overflow-x-auto flex md:justify-center">
+        <table className="border-2 border-gray-300 rounded-md p-2 mt-2 border-collapse overflow-x-auto">
           <thead>
             <tr>
-              <th className="p-2 border-2 border-gray-300 w-30">Fecha</th>
-              <th className="p-2 border-2 border-gray-300 w-30">Hora</th>
-              <th className="p-2 border-2 border-gray-300 w-50">Tipo</th>
-              <th className="p-2 border-2 border-gray-300 w-40">Director</th>
-              <th className="p-2 border-2 border-gray-300 w-40">Predicador</th>
+              { 
+                headers.map(header => (
+                  <th key={header.header} className={`p-2 border-2 text-lg ${header.style}`}>{header.header}</th>
+                ))
+              }
+              <th className="border-2 w-8 text-center align-middle"><svg className="m-auto"  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" /><path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z" /></svg></th>
             </tr>
           </thead>
+          <tbody>
+            {
+              items.map(item => (
+                <tr key={item.id} className="h-10">
+                  <td className="border-2 text-center align-middle">
+                    <input 
+                      className="w-28" 
+                      value={item.fecha} 
+                      onChange={(e) => modificar(e, "fecha", item)} 
+                      type="date"
+                    />
+                  </td>
+                  <td className="border-2 text-center align-middle">
+                    <input 
+                      className="w-28" 
+                      type="time"
+                      onChange={(e) => modificar(e, "hora", item)} 
+                    />
+                  </td>
+                  <td className="border-2 text-center align-middle">
+                    <input 
+                      className="text-center text-lg focus:outline-none" 
+                      type="text"  
+                      placeholder="Escuela Dominical.."
+                      onChange={(e) => modificar(e, "tipo", item)} 
+                    />
+                  </td>
+                  <td className="border-2 text-center align-middle">
+                    <select 
+                      className="focus:outline-none text-center text-lg" 
+                      name="director"
+                      onChange={(e) => modificar(e, "director", item)} 
+                    >
+                      {
+                        participantes.map(participante => (
+                          <option key={participante} value={participante}>{participante}</option>
+                        ))
+                      }
+                    </select>
+                  </td>
+                  <td className="border-2 text-center align-middle">
+                    <select 
+                      className="focus:outline-none text-center text-lg" 
+                      name="predicador"
+                      onChange={(e) => modificar(e, "predicador", item)}
+                    >
+                      {
+                        participantes.map(participante => (
+                          <option key={participante} value={participante}>{participante}</option>
+                        ))
+                      }
+                    </select>
+                  </td>
+                  <td className="border-2 hover:bg-red-600">
+                    <button 
+                      className="h-full flex justify-center items-center w-full cursor-pointer" 
+                      onClick={() => eliminarFila(item.id)}
+                    >
+                      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M20 6a1 1 0 0 1 .117 1.993l-.117 .007h-.081l-.919 11a3 3 0 0 1 -2.824 2.995l-.176 .005h-8c-1.598 0 -2.904 -1.249 -2.992 -2.75l-.005 -.167l-.923 -11.083h-.08a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" /><path d="M14 2a2 2 0 0 1 2 2a1 1 0 0 1 -1.993 .117l-.007 -.117h-4l-.007 .117a1 1 0 0 1 -1.993 -.117a2 2 0 0 1 1.85 -1.995l.15 -.005h4z" /></svg>
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
+      <button 
+        className="border-2 font-bold rounded-md p-2 px-5 mt-2 bg-blue-400 hover:bg-black hover:text-white cursor-pointer"
+        onClick={agregarFila}
+      >
+        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+      </button>
 
       <button 
         className="border-2 font-bold rounded-md p-2 px-5 mt-2 hover:bg-black hover:text-white cursor-pointer"
         onClick={handleSchedule}
-      >DESCARGAR</button>
+      >
+        DESCARGAR
+      </button>
 
     </div>
   )
---
+++ src/utils/data.ts
@@ -0,0 +1,38 @@
+const meses = [
+  "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+const headers = [
+  {
+    header: "Fecha",
+    style: "w-30"
+  }, 
+  {
+    header: "Hora",
+    style: "w-30"
+  }, 
+  {
+    header: "Tipo",
+    style: "w-50"
+  }, 
+  {
+    header: "Director",
+    style: "w-40"
+  }, 
+  {
+    header: "Predicador",
+    style: "w-40"
+  },
+]
+
+const participantes = [
+  "Participación",
+  "Pastor Armando",
+  "Pastora Stella",
+  "Rodrigo Marquez",
+  "Nohora E Sanchez",
+  "Juan P Marquez"
+]
+
+export {meses, headers, participantes};
