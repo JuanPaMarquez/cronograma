a874a1db120dcc16f4875961f47e1647787eaac7
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-04-14 19:29:16 -0500

    feat: initialize React app with TypeScript, Tailwind CSS, and SVG assets
    - Added main application layout in App.tsx
    - Configured entry point in main.tsx
    - Included Tailwind CSS styles in index.css
    - Created TypeScript configuration files for app and node
    - Set up Vite configuration with React and Tailwind CSS plugins
    - Added SVG assets for Vite and React logos

---
+++ src/layouts/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center h-screen bg-gray-200">
+      <h1 className="pt-5 text-2xl font-bold">Cronograma Iglesia del Zulia</h1>
+    </div>
+  )
+}
+
+export default App
